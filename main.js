a1727f07abdc1a1d3e658362aaa7a86a1f6a4d3c
speyyeti068 <75049648+speyyeti068@users.noreply.github.com>
2021-08-05 18:22:34 -0500

    Add files via upload

--- main.js
+++ main.js
@@ -1,16 +1,40 @@
 function preload() {
 	world_start = loadSound("world_start.wav");
+	marioJump = loadSound("jump.wav");
+	marioDie = loadSound("mariodie.wav")
+	marioGameOver = loadSound("gameover.wav")
+	marioCoin = loadSound("coin.wav");
+	marioKick = loadSound("kick.wav");
 	setSprites();
 	MarioAnimation();
 }
 
 function setup() {
-	canvas = createCanvas(1240,336);
+	canvas = createCanvas(1240, 336);
+	canvas.parent("canvas");
 	instializeInSetup(mario);
+	video = createCapture(VIDEO);
+	video.size(800, 400);
+	video.parent("game_console");
+	poseNet = ml5.poseNet(video,modelLoaded);
+	poseNet.on('pose',gotPoses);
+}
+
+function modelLoaded(){
+	console.log("Model Loaded")
+}
+
+function gotPoses(results){
+	if(results.length>0){
+		noseX = results[0].pose.nose.x;
+		noseY = results[0].pose.nose.y;
+
+		console.log(results);
+	}
 }
 
 function draw() {
-	game()
+	game();
 }
 
 
